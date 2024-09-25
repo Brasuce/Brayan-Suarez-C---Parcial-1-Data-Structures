b221d7e9a01977e24096bc2fb2e1d0c0f8a2bfa5
Brasuce <b.suarez@utp.edu.co>
2024-09-25 10:47:28 -0500

    Add files via upload

---
+++ AdjacencyList.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using namespace std;
+
+template <typename T> class adjacencyList {
+private:
+  class Node {
+  private:
+    T city;
+    Node *next;
+
+  public:
+    Node(T c) : city(c), next(nullptr) {}
+
+    T get_city() { return city; }
+
+    Node *get_next() { return next; }
+  };
+
+  class City {
+  private:
+    Node *connections;
+    City *next;
+
+  public:
+    City() : connections(nullptr), next(nullptr) {}
+  };
+
+  City *first;
+  int cityCount;
+
+public:
+  adjacencyList() : first(nullptr), cityCount(0) {}
+
+  void add_city() {
+    City *newCity = new City();
+    if (first == nullptr) {
+      first = newCity;
+    }
+    cityCount++;
+  }
+};
+
+int main() {
+    return 0;
+}
